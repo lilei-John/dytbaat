90dd35d48bb6053df60e78aeaccf7d71332f520e
Haukur Kristinssin <haukzinn@gmail.com>
2016-09-13 12:06:56 +0200

    implementing a cat

--- main.cpp
+++ main.cpp
@@ -1,19 +1,31 @@
 #include <iostream>
 #include "Dog.h"
+#include "Cat.h"
 
 using namespace std;
 
 int main() {
 
-    string name, barkSound;
+    string name, Sound, catOrDog;
+    cout << "Do you own a cat or a dog? ";
+    cin >> catOrDog;
+    if (catOrDog == "Dog" || catOrDog == "dog") {
+        cout << "What is the name of your dog? ";
+        cin >> name;
+        cout << "How does your dog bark? ";
+        cin >> Sound;
+        Dog myDawg(name, Sound);
+        cout << myDawg.bark() << endl;
+    }
+    else if (catOrDog == "Cat" || catOrDog == "cat") {
+        cout << "What is the name of your cat? ";
+        cin >> name;
+        cout << "How does your cat meow? ";
+        cin >> Sound;
+        Cat myCat(name, Sound);
+        cout << myCat.meow() << endl;
 
-    cout << "What is the name of your dog? ";
-    cin >> name;
-    cout << "How does your dog bark? ";
-    cin >> barkSound;
-
-    Dog myDawg(name, barkSound);
-    cout << myDawg.bark() << endl;
+    }
 
     return 0;
 }
