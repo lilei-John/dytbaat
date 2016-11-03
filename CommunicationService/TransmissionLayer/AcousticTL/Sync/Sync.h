54b0d7f6d724196cd7b59dfc29f995d699525981
Rasmus Haugaard <rasmus.l.haugaard@gmail.com>
2016-11-03 12:55:59 +0100

    implement sync fail callback

--- CommunicationService/TransmissionLayer/AcousticTL/Sync/Sync.h
+++ CommunicationService/TransmissionLayer/AcousticTL/Sync/Sync.h
@@ -8,6 +8,9 @@ public:
     void receiveNipple(unsigned char);
     bool startSequenceReceived();
     void reset();
+
+    void setOnSyncFailed(const std::function<void(int, const std::vector<unsigned char> &, unsigned char)> &onSyncFailed);
+
 private:
     FrameProtocol frameProtocol;
     const float searchPerTone;
@@ -18,4 +21,10 @@ private:
     int confStartNip = 0;
 
     unsigned char getStartNipple(int);
+
+    std::function<void(
+            int startSequenceFailIndex,
+            const std::vector<unsigned char> &startSequence,
+            unsigned char receivedByte
+    )> onSyncFailed;
 };
