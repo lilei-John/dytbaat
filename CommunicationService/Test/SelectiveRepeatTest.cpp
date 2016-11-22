24073e9fb2d7829817c2ba1693740e981bfb6ac2
Mikkel Borring Jakobsen <mikkeljakobsen@hotmail.com>
2016-11-22 15:59:11 +0100

    merged with master. Doesn't work...

--- CommunicationService/Test/SelectiveRepeatTest.cpp
+++ CommunicationService/Test/SelectiveRepeatTest.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+/* #include <iostream>
 #include "../CommunicationService.h"
 #include "../TransmissionLayer/AcousticTL/AcousticTL.h"
 #include "../Logger/Logger.h"
@@ -40,6 +40,10 @@ int main(){
     CommunicationService sender(outDLL, outTL, outRA);
     CommunicationService receiver(inDLL, inTL, inRA);
 
+    inTL.setOnFrameReceived([](vector<unsigned char> frame){
+        cout << "received frame" << endl;
+    });
+*/
 /*    outDLL.setOnTimeout([&](){
        logger.log("TIMEOUT");
     });
@@ -73,7 +77,7 @@ int main(){
 */
 
 
-
+/*
  //   std::this_thread::sleep_for(std::chrono::seconds(2));
     sender.transmit();
 
@@ -89,6 +93,111 @@ int main(){
     cout << "Test succeeded: " << (inData == outData) << endl;
     string result(inData.begin(), inData.end());
     cout << "Received text: " << result << endl;
+
+    return 0;
+}*/
+
+#include <iostream>
+#include "../CommunicationService.h"
+#include "../DataLinkLayer/SelectiveRepeat/SelectiveRepeat.h"
+#include "../TransmissionLayer/AcousticTL/AcousticTL.h"
+#include "../Logger/Logger.h"
+#include "../Media/RealAudio/RealAudio.h"
+
+using namespace std;
+
+int main(){
+    Logger logger("FrameTravelTimeTest");
+    vector<unsigned char> outData;
+    vector<unsigned char> inData;
+    string data = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet .";
+    for(int i = 0; i < data.size(); i++)
+        outData.push_back((unsigned char)data[i]);
+
+    cout << "(r)eceiver, (t)ransmitter or (b)oth ?" << endl;
+    string response;
+    getline(cin, response);
+    bool isReceiver = response == "r" || response == "b";
+    bool isTransmitter = response == "t" || response == "b";
+    if (!isReceiver && !isTransmitter){
+        cout << "Didn't understand command." << endl;
+        return 0;
+    }
+
+    int sampleRate = 44100;
+    float toneTime = 30; //ms
+    int samplesPerTone = (int)((float)sampleRate / 1000 * toneTime);
+
+    cout << "Samples per tone: " << samplesPerTone << endl;
+
+    stringstream outStream(ios::in|ios::out|ios::app);
+    stringstream inStream(ios::in|ios::out|ios::app);
+
+    SelectiveRepeat outDLL(outStream);
+    SelectiveRepeat inDLL(inStream);
+    AcousticTL outTL(sampleRate, samplesPerTone);
+    AcousticTL inTL(sampleRate, samplesPerTone);
+    RealAudio outRA(sampleRate);
+    RealAudio inRA(sampleRate);
+
+    CommunicationService sender(outDLL, outTL, outRA);
+    CommunicationService receiver(inDLL, inTL, inRA);
+
+
+    if (isReceiver){
+    /*    inDLL.setOnCrcFail([&](){
+            logger.log("RECEIVER CRC FAIL");
+        });
+        inDLL.setOnFlowFail([&](){
+            logger.log("RECEIVER FLOW FAIL");
+        });*/
+    }else{
+        receiver.disable();
+    }
+
+    long millisec;
+    if (isTransmitter){
+        for (auto byte : outData)
+            outStream << byte;
+   /*     outDLL.setOnTimeout([&](){
+            logger.log("TIMEOUT");
+        });
+        outDLL.setOnCrcFail([&](){
+            logger.log("SENDER CRC FAIL");
+        });
+        outDLL.setOnFlowFail([&](){
+            logger.log("SENDER FLOW FAIL");
+        });
+        outDLL.setOnFrameSendTime([&](){
+            chrono::milliseconds ms = chrono::duration_cast< chrono::milliseconds >(
+                    chrono::system_clock::now().time_since_epoch()
+            );
+            millisec = ms.count();
+        });
+        outDLL.setOnAckReceiveTime([&](){
+            chrono::milliseconds ms = chrono::duration_cast< chrono::milliseconds >(
+                    chrono::system_clock::now().time_since_epoch()
+            );
+            millisec = ms.count() - millisec;
+            logger.log("Frame travel time: " + to_string(millisec));
+        });*/
+        sender.transmit();
+    }else{
+        sender.disable();
+    }
+
+    cout << "Press enter to stop.." << endl;
+    cin.get();
+
+    if (isReceiver){
+        unsigned char index0;
+        while(inStream >> index0)
+            inData.push_back(index0);
+        cout << boolalpha << endl;
+        cout << "Test succeeded: " << (inData == outData) << endl;
+        string result(inData.begin(), inData.end());
+        cout << "Received text: " << result << endl;
+    }
 
     return 0;
 }
