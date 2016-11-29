fcab7ea45cc7fff5c15b57b0a14fb0906e0eb711
Rasmus Haugaard <rasmus.l.haugaard@gmail.com>
2016-11-29 13:19:16 +0100

    fix endless loop in escapeByteFrame

--- CommunicationService/TransmissionLayer/AcousticTL/FrameProtocol/FrameProtocol.h
+++ CommunicationService/TransmissionLayer/AcousticTL/FrameProtocol/FrameProtocol.h
@@ -10,12 +10,10 @@ public:
 
     void escapeByteFrame(std::vector<unsigned char> &);
 
-    const std::vector<unsigned char> &getStartBytes() const;
     unsigned char getStopByte() const;
     unsigned char getEscapeByte() const;
     bool shouldEscape(unsigned char) const;
 private:
-    std::vector<unsigned char> startBytes;
     unsigned char stopByte;
     unsigned char escapeByte;
 };
